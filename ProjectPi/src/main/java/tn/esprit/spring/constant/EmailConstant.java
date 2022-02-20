04c4b5b91bc1ebf3101837df6a711149416e2aa8
Iheb <44557368+iheb2@users.noreply.github.com>
2022-02-20 09:57:49 +0100

    Update EmailConstant.java

--- ProjectPi/src/main/java/tn/esprit/spring/constant/EmailConstant.java
+++ ProjectPi/src/main/java/tn/esprit/spring/constant/EmailConstant.java
@@ -2,8 +2,8 @@ package tn.esprit.spring.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "bigmario120@gmail.com";
-    public static final String PASSWORD = "admisadmis123A";
+    public static final String USERNAME = "***@***.com";
+    public static final String PASSWORD = "****";
     public static final String FROM_EMAIL = "support@getarrays.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
